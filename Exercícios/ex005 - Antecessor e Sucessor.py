56f5c8c82d37c36374e2dad415f275ba089609db
Thayná Cristina <134014880+thaynamarcelino@users.noreply.github.com>
2023-11-11 00:15:24 -0300

    adicionei mais exercicios

---
+++ Exercícios/ex005 - Antecessor e Sucessor.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n = int(input('Digite um numero: '))
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, (n-1), (n+1)))
---
+++ Exercícios/ex006 - Dobro, Triplo, Raiz Quadrada.py
@@ -0,0 +1,5 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n = int(input('Digite um número: '))
+print('O dobro de {} vale {}.'.format(n, (n*2)))
+print('O triplo de {} vale {}. \nA raiz quadrada de {} é igual a {:.2f}.'.format(n, (n*3), n, pow(n, (1/2))))
