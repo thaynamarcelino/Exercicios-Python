5c3b9b0847d65adc238a8951bec43b585911a70b
Thayná Cristina <140473935+thaynamarcelino@users.noreply.github.com>
2023-11-14 00:18:31 -0300

    Update ex009 - Tabuada.py

--- Exercícios/ex009 - Tabuada.py
+++ Exercícios/ex009 - Tabuada.py
@@ -1,3 +1,5 @@
+# Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
 num = int(input('Digite um número para ver sua tabuada: '))
 print('-' * 12)
 print('{} X {:2} = {}'.format(num, 1, num*1))
