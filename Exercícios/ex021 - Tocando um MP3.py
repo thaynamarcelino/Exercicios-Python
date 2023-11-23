61bae27fbfb04bef167ccce6ce1999fd03f02614
Thayná Cristina <140473935+thaynamarcelino@users.noreply.github.com>
2023-11-22 23:08:48 -0300

    Update ex021 - Tocando um MP3.py

--- Exercícios/ex021 - Tocando um MP3.py
+++ Exercícios/ex021 - Tocando um MP3.py
@@ -1,5 +1,7 @@
+# Faça um programa em python que abra e reproduza o áudio de um arquivo MP3.
+
 import pygame
 pygame.init()
-pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.load('# aqui coloca o nome do arquivo mp3') 
 pygame.mixer.music.play()
 pygame.event.wait()
