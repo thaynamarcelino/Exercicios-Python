e66bf5c296247f9e028f8b0b2931716fcfb3c6cf
Thayná Cristina <140473935+thaynamarcelino@users.noreply.github.com>
2023-12-12 23:38:20 -0300

    Update ex024 - Verificando as primeiras letras de um texto.py

--- Exercícios/ex024 - Verificando as primeiras letras de um texto.py
+++ Exercícios/ex024 - Verificando as primeiras letras de um texto.py
@@ -1,2 +1,4 @@
+# Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
+
 cid = str(input('Em que cidade você nasceu? ')).strip()
 print(cid[0:5].upper() == 'SANTO')
