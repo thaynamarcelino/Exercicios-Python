063e535f2fd480b09645041b8120a58b1ae4b6e4
Thayná Cristina <140473935+thaynamarcelino@users.noreply.github.com>
2023-12-26 19:05:53 -0300

    Update ex030  - Par ou Ímpar.py

--- Exercícios/ex030  - Par ou Ímpar.py
+++ Exercícios/ex030  - Par ou Ímpar.py
@@ -1,3 +1,5 @@
+# Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
 número = int(input('Me diga um número qualquer: '))
 resultado = número % 2
 if resultado == 0:
