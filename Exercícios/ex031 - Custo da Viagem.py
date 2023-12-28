f445c62dbe952e337bbee1da59be3c945e34e048
Thayná Cristina <140473935+thaynamarcelino@users.noreply.github.com>
2023-12-27 21:40:12 -0300

    Update ex031 - Custo da Viagem.py

--- Exercícios/ex031 - Custo da Viagem.py
+++ Exercícios/ex031 - Custo da Viagem.py
@@ -1,3 +1,5 @@
+# Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 para viagens mais longas.
+
 distância = float(input('Qual é a distância da sua viagem? '))
 print('Você está prestes a começar uma viagem de {}Km.'.format(distância))
 if distância <= 200:
