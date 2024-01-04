10a5dcaee4d649ed6cd4caed4ed9eed2abe465e4
Thayná Cristina <140473935+thaynamarcelino@users.noreply.github.com>
2024-01-04 09:13:44 -0300

    Update ex032 - Ano Bissexto.py

--- Exercícios/ex032 - Ano Bissexto.py
+++ Exercícios/ex032 - Ano Bissexto.py
@@ -1,3 +1,5 @@
+# Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
+
 from datetime import date
 ano = int(input('Que ano quer analisar? Coloque 0 para analisar o ano atual: '))
 if ano == 0:
