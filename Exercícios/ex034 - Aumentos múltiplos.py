6ed1fafae390f7591bc4d2731cf980216322fa01
Thayná Cristina <140473935+thaynamarcelino@users.noreply.github.com>
2024-01-04 12:15:34 -0300

    Update ex034 - Aumentos múltiplos.py

--- Exercícios/ex034 - Aumentos múltiplos.py
+++ Exercícios/ex034 - Aumentos múltiplos.py
@@ -1,3 +1,5 @@
+# Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores a R$1.250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
+
 salário = float(input('Qual é o salário do funcionário? R$'))
 if salário <= 1250:
     novo = salário + (salário * 15 / 100)
