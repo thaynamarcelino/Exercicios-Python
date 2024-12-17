679e1d56c2590ced2123ae274e2a7bc50431d6bb
Thayná Cristina <140473935+thaynamarcelino@users.noreply.github.com>
2024-12-17 00:21:38 -0300

    Update ex037 - Conversor de bases numéricas.py

--- Exercícios/ex037 - Conversor de bases numéricas.py
+++ Exercícios/ex037 - Conversor de bases numéricas.py
@@ -11,7 +11,7 @@ print('''Escolha uma das bases para conversão
 [3] converter para HEXADECIMAL''')
 opção = int(input('Sua opção: '))
 if opção == 1:
-    print('{} convertido para BINÉRIO é igual a {}'.format(num, bin(num)[2:]))
+    print('{} convertido para BINÁRIO é igual a {}'.format(num, bin(num)[2:]))
 elif opção == 2:
     print('{} convertido para OCTAL é igual a {}'.format(num, oct(num)[2:]))
 elif opção == 3:
